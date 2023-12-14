1811a9d48b379e4609b7d4e70aa4b344c8c00ea0
shalom78b <153871538+shalom78b@users.noreply.github.com>
2023-12-15 00:01:07 +0100

    Add files via upload
    class exercise G1

---
+++ facebook/home.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+if (isset($_SESSION["email"])) {
+    $email = $_SESSION["email"];
+    session_write_close();
+} else {
+    // since the username is not set in session, the user is not-logged-in
+    // he is trying to access this page unauthorized
+    // so let's clear all session variables and redirect him to index
+    session_unset();
+    session_write_close();
+    $url = "./index.php";
+    header("Location: $url");
+}
+
+?>
+<HTML>
+<HEAD>
+<TITLE>Welcome</TITLE>
+<link href="assets/css/phppot-style.css" type="text/css"
+	rel="stylesheet" />
+<link href="assets/css/user-registration.css" type="text/css"
+	rel="stylesheet" />
+</HEAD>
+<BODY>
+	<div class="phppot-container">
+		<div class="page-header">
+			<span class="login-signup"><a href="logout.php">Logout</a></span>
+		</div>
+		<div class="page-content">Welcome to facebook, connect with friends and the world<?php echo $username;?><br><br>
+          <h1>facebook is a place for interraction</h1>
+          <h2>users</h2>
+    
+        </div>
+	</div>
+    <div>
+    <?php 
+    public function getMember($username)
+    {
+        $query = 'SELECT * FROM user';
+        $paramType = 's';
+        $paramValue = array(
+            $username
+        );
+        $memberRecord = $this->ds->select($query, $paramType, $paramValue);
+        return $memberRecord;
+    } 
+    ?>
+    </div>
+</BODY>
+</HTML>
---
+++ facebook/model/Member.php
@@ -0,0 +1,158 @@
+<?php
+namespace Phppot;
+
+class Member
+{
+
+    private $ds;
+
+    function __construct()
+    {
+        require_once __DIR__ . '/../lib/DataSource.php';
+        $this->ds = new DataSource();
+    }
+
+    /**
+     * to check if the username already exists
+     *
+     * @param string $username
+     * @return boolean
+     */
+    public function isUsernameExists($username)
+    {
+        $query = 'SELECT * FROM user where username = ?';
+        $paramType = 's';
+        $paramValue = array(
+            $username
+        );
+        $resultArray = $this->ds->select($query, $paramType, $paramValue);
+        $count = 0;
+        if (is_array($resultArray)) {
+            $count = count($resultArray);
+        }
+        if ($count > 0) {
+            $result = true;
+        } else {
+            $result = false;
+        }
+        return $result;
+    }
+
+    /**
+     * to check if the email already exists
+     *
+     * @param string $email
+     * @return boolean
+     */
+    public function isEmailExists($email)
+    {
+        $query = 'SELECT * FROM user where email = ?';
+        $paramType = 's';
+        $paramValue = array(
+            $email
+        );
+        $resultArray = $this->ds->select($query, $paramType, $paramValue);
+        $count = 0;
+        if (is_array($resultArray)) {
+            $count = count($resultArray);
+        }
+        if ($count > 0) {
+            $result = true;
+        } else {
+            $result = false;
+        }
+        return $result;
+    }
+
+    /**
+     * to signup / register a user
+     *
+     * @return string[] registration status message
+     */
+    public function registerMember()
+    {
+        $isUsernameExists = $this->isUsernameExists($_POST["username"]);
+        $isEmailExists = $this->isEmailExists($_POST["email"]);
+        if ($isUsernameExists) {
+            $response = array(
+                "status" => "error",
+                "message" => "Username already exists."
+            );
+        } else if ($isEmailExists) {
+            $response = array(
+                "status" => "error",
+                "message" => "Email already exists."
+            );
+        } else {
+            if (! empty($_POST["signup-password"])) {
+
+                // PHP's password_hash is the best choice to use to store passwords
+                // do not attempt to do your own encryption, it is not safe
+                $hashedPassword = password_hash($_POST["signup-password"], PASSWORD_DEFAULT);
+            }
+            $query = 'INSERT INTO user (username, password, email, age, tel) VALUES (?, ?, ?, ?, ?)';
+            $paramType = 'sssss';
+            $paramValue = array(
+                $_POST["username"],
+                $hashedPassword,
+                $_POST["email"],
+                $_POST["age"],
+                $_POST["tel"]
+            );
+            $memberId = $this->ds->insert($query, $paramType, $paramValue);
+            if (! empty($memberId)) {
+                $response = array(
+                    "status" => "success",
+                    "message" => "You have registered successfully."
+                );
+            }
+        }
+        return $response;
+    }
+
+    public function getMember($email)
+    {
+        $query = 'SELECT * FROM user where EMAIL = ?';
+        $paramType = 's';
+        $paramValue = array(
+            $email
+        );
+        $memberRecord = $this->ds->select($query, $paramType, $paramValue);
+        return $memberRecord;
+    }
+
+    /**
+     * to login a user
+     *
+     * @return string
+     */
+    public function loginMember()
+    {
+        $memberRecord = $this->getMember($_POST["email"]);
+        $loginPassword = 0;
+        if (! empty($memberRecord)) {
+            if (! empty($_POST["login-password"])) {
+                $password = $_POST["login-password"];
+            }
+            $hashedPassword = $memberRecord[0]["password"];
+            $loginPassword = 0;
+            if (password_verify($password, $hashedPassword)) {
+                $loginPassword = 1;
+            }
+        } else {
+            $loginPassword = 0;
+        }
+        if ($loginPassword == 1) {
+            // login sucess so store the member's username in
+            // the session
+            session_start();
+            $_SESSION["email"] = $memberRecord[0]["email"];
+            session_write_close();
+            $url = "./home.php";
+            header("Location: $url");
+        } else if ($loginPassword == 0) {
+            $loginStatus = "Invalid email or password.";
+            return $loginStatus;
+        }
+    }
+}
